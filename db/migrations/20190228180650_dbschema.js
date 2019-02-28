d52f67afb07b6ccb1fbba3ebc6990241c8f727a2
Patrick O Connor <padraigoc@gmail.com>
2019-02-28 21:16:51 +0000

    Updating seed files with test data

--- db/migrations/20190228180650_dbschema.js
+++ db/migrations/20190228180650_dbschema.js
@@ -1,28 +1,22 @@
 //applying migration
 exports.up = function(knex, Promise) {
-    // return knex.schema.createTable('users', (table) => {
-    //     table.increments();
-    //     table.string('username');
-    //     table.string('password');
-    //     table.string('first_name');
-    //     table.string('last_name');
-    //   }).then(createPostTable)
     return createPostTable()
         .then(createUsersTable)
         .catch(err => console.log(err));
 
     function createPostTable () {
         return knex.schema.createTable('posts', (table) => {
-            table.increments();
-            table.string('title');
-            table.string('description');
+            table.increments().primary();
+            table.string('title', 20);
+            table.string('description', 800);
+            table.string('URL');
             table.date('create_time');
         })
     }
 
     function createUsersTable () {
         return knex.schema.createTable('users', (table) => {
-            table.increments();
+            table.increments().primary();
             table.string('username');
             table.string('password');
             table.string('first_name');
