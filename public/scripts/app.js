77df3c0a0a3de81c6342ce9481b8bc262c25e3bc
Madhuri Chalamangalam <madhuri.chalamangalam@gmail.com>
2019-03-01 03:01:10 +0000

    updated app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,21 @@
 
 $(() => {
+  function createPost(post) {
+    return $(`<div class="post" >
+      <p>${post.title}</p>
+      <p>${post.description}</p>
+      <p>${post.URL}</p>
+    </div>`)
+  }
+
+  function renderPosts(posts) {
+    $('#post-container').html('')
+
+    posts.forEach(post => {
+      $('#post-container').prepend(createPost(post))
+    })
+  }
+
   $.ajax({
     method: "GET",
     url: "/api/users"
@@ -7,9 +23,14 @@ $(() => {
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
+  });
 
+  $.ajax({
+    method: "GET",
+    url: "/api/posts"
+  }).done((posts) => {
+    console.log(posts)
+    renderPosts(posts)
+  })
 
-});
-
-
+})
