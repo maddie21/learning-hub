83fcfbb5a86ccb2b9eda29d585a98c90192feed3
xw <23066125+xwang1000@users.noreply.github.com>
2019-03-01 22:55:21 -0800

    Revert "Improve writing style."
    This reverts commit 88e2ce5304735a4136535f3aad90d7e3f6452818.

--- db/migrations/20190228180650_dbschema.js
+++ db/migrations/20190228180650_dbschema.js
@@ -1,78 +1,77 @@
 //applying migration
-exports.up = function (knex, Promise) {
-  return createUsersTable()
-    .then(createCategoriesTable)
-    .then(createPostTable)
-    .then(createCommentsTable)
-    .then(createPostMetadataTable)
-    .then(createPostCategoriesTable)
-    .catch(err => console.log(err));
+exports.up = function(knex, Promise) {
+    return createUsersTable()
+        .then(createPostTable)
+        .then(createCategoriesTable)
+        .then(createCommentsTable)
+        .then(createPostMetadataTable)
+        .then(createPostCategoriesTable)
+        .catch(err => console.log(err));
 
-  function createUsersTable() {
-    return knex.schema.createTable('users', (table) => {
-      table.increments().primary();
-      table.string('username');
-      table.string('password');
-      table.string('first_name');
-      table.string('last_name');
-    })
-  }
+        function createUsersTable () {
+            return knex.schema.createTable('users', (table) => {
+                table.increments().primary();
+                table.string('username');
+                table.string('password');
+                table.string('first_name');
+                table.string('last_name');
+            })
+        }
+        
+        function createPostTable () {
+            return knex.schema.createTable('posts', (table) => {
+              table.increments().primary();
+              table.string('title', 30);
+              table.string('description', 1000);
+              table.string('URL');
+              table.date('create_time');
+              table.integer('user_id').references('id').inTable('users');
+            })
+        }
 
-  function createPostTable() {
-    return knex.schema.createTable('posts', (table) => {
-      table.increments().primary();
-      table.string('title', 30);
-      table.string('description', 200);
-      table.string('URL');
-      table.date('create_time');
-      table.integer('user_id').references('id').inTable('users');
-      table.integer('category_id').references('id').inTable('categories');
-    })
-  }
+        function createCategoriesTable () {
+            return knex.schema.createTable('categories', (table) => {
+              table.increments().primary();
+              table.string('category_name')
+            })
+        }
 
-  function createCategoriesTable() {
-    return knex.schema.createTable('categories', (table) => {
-      table.increments().primary();
-      table.string('category_name')
-    })
-  }
+        function createCommentsTable () {
+            return knex.schema.createTable('comments', (table) => {
+              table.increments().primary();
+              table.string('content');
+              table.date('create_time');
+              table.integer('user_id').references('id').inTable('users');
+              table.integer('post_id').references('id').inTable('posts');
+            })
+        }
 
-  function createCommentsTable() {
-    return knex.schema.createTable('comments', (table) => {
-      table.increments().primary();
-      table.string('content');
-      table.date('create_time');
-      table.integer('user_id').references('id').inTable('users');
-      table.integer('post_id').references('id').inTable('posts');
-    })
-  }
+        function createPostMetadataTable () {
+            return knex.schema.createTable('post_metadata', (table) => {
+              table.increments().primary();
+              table.integer('like');
+              table.integer('rating');
+              table.integer('user_id').references('id').inTable('users');
+              table.integer('post_id').references('id').inTable('posts');
+            })
+        }
 
-  function createPostMetadataTable() {
-    return knex.schema.createTable('post_metadata', (table) => {
-      table.increments().primary();
-      table.integer('like');
-      table.integer('rating');
-      table.integer('user_id').references('id').inTable('users');
-      table.integer('post_id').references('id').inTable('posts');
-    })
-  }
+        function createPostCategoriesTable () {
+            return knex.schema.createTable('post_categories', (table) => {
+                table.increments().primary();
+                table.integer('categories_id').references('id').inTable('categories');
+                table.integer('post_id').references('id').inTable('posts');        
+            })
+        }
 
-  function createPostCategoriesTable() {
-    return knex.schema.createTable('post_categories', (table) => {
-      table.increments().primary();
-      table.integer('categories_id').references('id').inTable('categories');
-      table.integer('post_id').references('id').inTable('posts');
-    })
-  }
-
-};
+  };
 
 //rolling back migration 
-exports.down = function (knex, Promise) {
-  return knex.schema.dropTable('users')
-    .then(knex.schema.dropTable('posts'))
-    .then(knex.schema.dropTable('categories'))
-    .then(knex.schema.dropTable('comments'))
-    .then(knex.schema.dropTable('post_metadata'))
-    .then(knex.schema.dropTable('post_categories'));
+exports.down = function(knex, Promise) {
+ return knex.schema.dropTable('users')
+ .then(knex.schema.dropTable('posts'))
+ .then(knex.schema.dropTable('categories'))
+ .then(knex.schema.dropTable('comments'))
+ .then(knex.schema.dropTable('post_metadata'))
+ .then(knex.schema.dropTable('post_categories'));   
 };
--- db/seeds/02_posts.js
+++ db/seeds/02_posts.js
@@ -8,38 +8,34 @@ exports.seed = function(knex, Promise) {
         knex('posts')
         .insert({
                 title: 'Pancakes', 
-                description: 'yumm', 
+                description: 'yummmmmmmmmmmmmmmmmmmmm ', 
                 URL: 'https://www.allrecipes.com/recipe/21014/good-old-fashioned-pancakes/' }),
 
         knex('posts')
         .insert({
                 title: 'Samsung Fold Phone', 
                 description: 'Samsung Galaxy Fold is an upcoming Android hybrid smartphone developed by Samsung Electronics. Unveiled February 21, 2019, it will first be released in the United States on April 26, 2019, and in Europe on May 3, 2019. ', 
-                URL: 'https://www.youtube.com/watch?v=kK2W7ErTF3g'
-        }),
+                URL: 'https://www.youtube.com/watch?v=kK2W7ErTF3g'}),
 
         knex('posts')
         .insert({
                 title: 'Chocolate Fudge Cake', 
                 description: 'For the true chocolate lover, this cake is moist and dense and you are going to love it. Great with cream cheese frosting or cooked fudge frosting.', 
-                URL: 'https://www.allrecipes.com/recipe/7374/fudge-cake/' 
-        }),
+                URL: 'https://www.allrecipes.com/recipe/7374/fudge-cake/' }),
 
         knex('posts')
         .insert({
                 title: 'Superbowl 2019', 
                 description: 'Super Bowl LIII was an American football game between the three-time defending American Football Conference (AFC) champions New England Patriots and the National Football Conference (NFC) champion Los Angeles Rams', 
                 URL: 'https://en.wikipedia.org/wiki/Super_Bowl_LIII',
-                create_time:  knex.raw('CURRENT_TIMESTAMP')
-        }),
+                create_time:  knex.raw('CURRENT_TIMESTAMP') }),
 
         knex('posts')
         .insert({
                 title: 'NY Fashion Week', 
                 description: 'Palo santo keffiyeh tilde', 
                 URL: 'http://nyfw.com/',
-                create_time:  knex.raw('CURRENT_TIMESTAMP')
-        })
+                create_time:  knex.raw('CURRENT_TIMESTAMP') }),
       ]);
     });
 };
