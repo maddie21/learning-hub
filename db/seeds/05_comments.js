70082fe929879a8c061497dde44f966ad4366f1e
xw <23066125+xwang1000@users.noreply.github.com>
2019-03-04 08:05:45 -0800

    Update 05_comments.js

--- db/seeds/05_comments.js
+++ db/seeds/05_comments.js
@@ -6,12 +6,12 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('comments').insert({content: 'Wow, this is so cool!' , create_time: knex.raw('CURRENT_TIMESTAMP') , post_id: 1 , user_id: 1 }),
-        knex('comments').insert({content: 'Yummmmy! :)' , create_time: knex.raw('CURRENT_TIMESTAMP') , post_id: 1 , user_id: 3 }),
-        knex('comments').insert({content: 'Mmmmmm! these look good' , create_time: knex.raw('CURRENT_TIMESTAMP') , post_id: 1 , user_id: 4 }),
-        knex('comments').insert({content: 'Wow! interesting read :)' , create_time: knex.raw('CURRENT_TIMESTAMP') , post_id: 2 , user_id: 2 }),
+        knex('comments').insert({content: 'Thanks for sharing this knowledge :)' , create_time: knex.raw('CURRENT_TIMESTAMP') , post_id: 1 , user_id: 3 }),
+        knex('comments').insert({content: 'Actually, I have an opposite theory...' , create_time: knex.raw('CURRENT_TIMESTAMP') , post_id: 1 , user_id: 4 }),
+        knex('comments').insert({content: 'Cool stuff man.' , create_time: knex.raw('CURRENT_TIMESTAMP') , post_id: 2 , user_id: 2 }),
         knex('comments').insert({content: 'Really cool video!' , create_time: knex.raw('CURRENT_TIMESTAMP') , post_id: 2 , user_id: 3 }),
-        knex('comments').insert({content: 'I am going to make this :)' , create_time: knex.raw('CURRENT_TIMESTAMP') , post_id: 3 , user_id: 2 }),
-        knex('comments').insert({content: 'Yum yum :)' , create_time: knex.raw('CURRENT_TIMESTAMP') , post_id: 3 , user_id: 4 }),
+        knex('comments').insert({content: '2019 and still reading this.' , create_time: knex.raw('CURRENT_TIMESTAMP') , post_id: 3 , user_id: 2 }),
+        knex('comments').insert({content: 'I wonder that too!' , create_time: knex.raw('CURRENT_TIMESTAMP') , post_id: 3 , user_id: 4 }),
       ]);
     });
 };
